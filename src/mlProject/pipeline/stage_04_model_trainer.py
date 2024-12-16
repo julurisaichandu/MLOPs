4702f92e92e64106f628737300c81f5a576d7b72
julurisaichandu <juluri.saichandu99@gmail.com>
2024-12-15 20:32:24 -0500

    1. added CI CD for the wine classification dataset 2. using docker and aws for deployment 3. mlfow not pushed into repo, however it will be in the next push

---
+++ src/mlProject/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,29 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.model_trainer import ModelTrainer
+from mlProject import logger
+
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+if __name__ == "__main__":
+    STAGE_NAME = "Model Trainer stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_ingestion = ModelTrainerTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
